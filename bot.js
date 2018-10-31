f46fec7576e2594853262a49e8d1206d95ac711e
NerLOR#0001 <36960271+mraco@users.noreply.github.com>
2018-10-31 12:12:10 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -256,7 +256,7 @@ function play(guild, song) {
 }
 });
 client.on('message' , message => {
-  var prefix = "*";
+  var prefix = "N";
   if(message.author.bot) return;
  
   if(message.content.startsWith(prefix + "xo")) {
